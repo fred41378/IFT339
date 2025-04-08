96a9da5c5b730704e3a147f3665be8c83e5a10c6
Frederick <tayf2335@usherbrooke.ca>
2025-04-08 03:55:03 -0400

    TP5 ALL OF IT

---
+++ TP5_IFT339/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <map>
+
+#include "map.h"
+
+int main() {
+    std::string lignesSeparation = "\n====================================";
+
+
+    std::cout << lignesSeparation;
+    std::cout << "\nTEST INSERT PAR CLEF";
+    std::cout << lignesSeparation << std::endl;
+
+    map<int,std::string> map_insert_clef;
+    std::cout << "Avant insert :" << std::endl;
+    map_insert_clef.afficher();
+    map_insert_clef.insert(10);
+    map_insert_clef.insert(20);
+    map_insert_clef.insert(30);
+    std::cout << "Apres insert, mais sans contenu : " << std::endl;
+    map_insert_clef.afficher();
+    map_insert_clef[10] = "Dix";
+    map_insert_clef[20] = "Vingt";
+    map_insert_clef[30] = "Trente";
+    std::cout << "Apres insert, avec contenu : " << std::endl;
+    map_insert_clef.afficher();
+
+
+    std::cout << lignesSeparation;
+    std::cout << "\nTEST INSERT PAR ITERRATEUR";
+    std::cout << lignesSeparation << std::endl;
+
+    map<int,std::string> map_insert_iterator;
+    std::cout << "Avant insert :" << std::endl;
+    map_insert_iterator.afficher();
+    map_insert_iterator.insert(10);
+    map_insert_iterator.insert(20);
+    map_insert_iterator.insert(30);
+    map_insert_iterator[10] = "Dix";
+    map_insert_iterator[20] = "Vingt";
+    map_insert_iterator[30] = "Trente";
+    std::cout << "Apres insert regulier :" << std::endl;
+    map_insert_iterator.afficher();
+    auto indice1 = map_insert_iterator.find(20);
+    map_insert_iterator.insert(indice1, 15);
+    map_insert_iterator[15] = "Quinze";
+    std::cout << "Apres insert avec iterator (15) : " << std::endl;
+    map_insert_iterator.afficher();
+
+
+    std::cout << lignesSeparation;
+    std::cout << "\nTEST ERASE PAR CLEF";
+    std::cout << lignesSeparation << std::endl;
+
+    map<int,std::string> map_erase_clef;
+    map_erase_clef.insert(10);
+    map_erase_clef[10] = "Dix";
+    map_erase_clef.insert(20);
+    map_erase_clef[20] = "Vingt";
+    map_erase_clef.insert(30);
+    map_erase_clef[30] = "Trente";
+    map_erase_clef.insert(40);
+    map_erase_clef[40] = "Quarante";
+    std::cout << "Avant erase :" << std::endl;
+    map_erase_clef.afficher();
+    map_erase_clef.erase(20);
+    map_erase_clef.afficher();
+
+    return 0;
+}
---
+++ TP5_IFT339/map2.h
@@ -0,0 +1,156 @@
+/**
+* \ file set2.h
+* \ author Aida Ouangraoua (Mars. 2025)
+* \ brief Ce fichier contient les fonctions 
+* \ du type map à coder
+*/
+
+
+#ifndef map2_h
+#define map2_h
+
+
+/////////////////////////////////////////////////
+// map
+// fonctions publiques
+/////////////////////////////////////////////////
+
+template <typename Tclef, typename Tvaleur>
+typename map<Tclef,Tvaleur>::iterator map<Tclef,Tvaleur>::lower_bound(const Tclef& c) const {
+  noeud* n = APRES->GAUCHE;
+  noeud* lower = APRES;
+
+  while (n != nullptr) {
+    if (!(n->CONTENU->first >= c)) {
+      lower = n;
+      n = n->GAUCHE;
+    } else {
+      n = n->DROITE;
+    }
+  }
+
+  return iterator(lower);
+}
+
+
+template <typename Tclef, typename Tvaleur>
+typename map<Tclef,Tvaleur>::iterator map<Tclef,Tvaleur>::insert(iterator j, const Tclef& c) {
+  if (j == end()) {
+    if (!empty()) {
+      noeud* noeud_max = APRES->GAUCHE;
+      while (noeud_max->DROITE != nullptr) {
+        noeud_max = noeud_max->DROITE;
+      }
+      if (c > noeud_max->CONTENU->first) {
+        iterator it;
+        if (ajoute_droite(c, noeud_max, it)) {
+          return it;
+        }
+      }
+    }
+    return insert(c).first;
+  } else {
+    noeud* indice_noeud = j.POINTEUR;
+    if (indice_noeud->CONTENU->first == c) {
+      return j;
+    }
+    if (c < indice_noeud->CONTENU->first) {
+      iterator prec = j;
+      --prec;
+      if (prec == end() || prec->first < c) {
+        iterator it;
+        if (ajoute_gauche(c, indice_noeud, it)) {
+          return it;
+        }
+      }
+    } else {
+      iterator suiv = j;
+      ++suiv;
+      if (suiv == end() || suiv->first > c) {
+        iterator it;
+        if (ajoute_droite(c, indice_noeud, it)) {
+          return it;
+        }
+      }
+    }
+    return insert(c).first;
+  }
+}
+
+template <typename Tclef, typename Tvaleur>
+  size_t map<Tclef,Tvaleur>::erase(const Tclef& c){
+  if (empty()) return 0;
+
+  noeud* noeud_a_remplacer = nullptr;
+  noeud* racine = APRES->GAUCHE;
+
+  bool erased = erase(c, racine, noeud_a_remplacer);
+
+  if (erased == true) {
+    APRES->GAUCHE = racine;
+    return 1;
+  }
+  return 0;
+}
+
+template <typename Tclef, typename Tvaleur>
+  typename map<Tclef,Tvaleur>::iterator map<Tclef,Tvaleur>::erase(iterator i){
+  if (i == end()) return end();
+
+  iterator it = i;
+  ++it;
+
+  Tclef c = i->first;
+  erase(c);
+
+  return it;
+}
+
+
+///////////////////////////////////////////////////
+// fonctions privees pour la gestion de l'equilibre
+///////////////////////////////////////////////////
+
+
+template <typename Tclef, typename Tvaleur>
+void map<Tclef,Tvaleur>::rotation_gauche_droite(noeud*& p){
+  noeud* c = p->GAUCHE;
+  noeud* c_droite = c->DROITE;
+
+  c->DROITE = p;
+  c->PARENT = p->PARENT;
+  p->PARENT = c;
+
+  p->GAUCHE = c_droite;
+  if (c_droite != nullptr) {
+    c_droite->PARENT = p;
+  }
+
+  p=c;
+
+  p->INDICE=0;
+  p->DROITE->INDICE=0;
+}
+
+template <typename Tclef, typename Tvaleur>
+void map<Tclef,Tvaleur>::rotation_droite_gauche(noeud*& p){
+  noeud* c = p->DROITE;
+  noeud* c_gauche = c->GAUCHE;
+
+  c->GAUCHE = p;
+  c->PARENT = p->PARENT;
+  p->PARENT = c;
+
+  p->DROITE = c_gauche;
+  if (c_gauche != nullptr) {
+    c_gauche->PARENT = p;
+  }
+
+  p=c;
+
+  p->INDICE=0;
+  p->GAUCHE->INDICE=0;
+}
+
+
+#endif /* map2_h */
